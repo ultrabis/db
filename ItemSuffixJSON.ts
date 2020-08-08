09c7279f1478bf3ab3f2c91ecb0e6cf3cb9cf61e
kmmiles <kmmiles@users.noreply.github.com>
2020-08-08 21:30:11 +0000

    deploy: fae81d3b864f8fcc3dc1b10f81d786c1266c7b63

--- ItemSuffixJSON.ts
+++ ItemSuffixJSON.ts
@@ -1,5 +1,5 @@
 export default interface ItemSuffixJSON {
   id: number
   type: number
-  bonus: [{ type: number; value: number }]
+  bonus: [{ type: number; value: number }, { type: number; value: number }]
 }
